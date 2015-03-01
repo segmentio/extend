9e1b84e90aa2a826cacf14884f9abd9d3939693d
Tomas Sardyha <darsain@gmail.com>
2015-03-01 14:26:57 +0100

    Skip falsy arguments.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function extend (object) {
     var args = Array.prototype.slice.call(arguments, 1);
 
     // For each extender, copy their properties on our object.
-    for (var i = 0, source; source = args[i]; i++) {
+    for (var i = 0, l = args.length, source; source = args[i], i < l; i++) {
         if (!source) continue;
         for (var property in source) {
             object[property] = source[property];
